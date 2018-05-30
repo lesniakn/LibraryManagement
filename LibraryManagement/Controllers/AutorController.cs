00fe3a21b454d0528b1f61b8194a9166491ab24d
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-30 14:25:38 +0200

    Utworzenie kontrolera Autor i widoku dla niego

---
+++ LibraryManagement/Controllers/AutorController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagement.Models;
+
+namespace LibraryManagement.Controllers
+{
+    public class AutorController : Controller
+    {
+        private LibraryManagementDataEntities db = new LibraryManagementDataEntities();
+
+        // GET: Autor
+        public ActionResult Index()
+        {
+            return View(db.Autor.ToList());
+        }
+
+        // GET: Autor/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autor.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // GET: Autor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Imie,Nazwisko")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Autor.Add(autor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(autor);
+        }
+
+        // GET: Autor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autor.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // POST: Autor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Imie,Nazwisko")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(autor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(autor);
+        }
+
+        // GET: Autor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autor.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // POST: Autor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Autor autor = db.Autor.Find(id);
+            db.Autor.Remove(autor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
