8d40aa2b41a30a34c5362b17611e0aacecaef16c
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-30 14:27:19 +0200

    Utworzenie kontrolera Czytelnik i widoku dla niego

---
+++ LibraryManagement/Controllers/CzytelnikController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagement.Models;
+
+namespace LibraryManagement.Controllers
+{
+    public class CzytelnikController : Controller
+    {
+        private LibraryManagementDataEntities db = new LibraryManagementDataEntities();
+
+        // GET: Czytelnik
+        public ActionResult Index()
+        {
+            return View(db.Czytelnik.ToList());
+        }
+
+        // GET: Czytelnik/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Czytelnik czytelnik = db.Czytelnik.Find(id);
+            if (czytelnik == null)
+            {
+                return HttpNotFound();
+            }
+            return View(czytelnik);
+        }
+
+        // GET: Czytelnik/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Czytelnik/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Imie,Nazwisko,Uzytkownik,Haslo,Email")] Czytelnik czytelnik)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Czytelnik.Add(czytelnik);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(czytelnik);
+        }
+
+        // GET: Czytelnik/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Czytelnik czytelnik = db.Czytelnik.Find(id);
+            if (czytelnik == null)
+            {
+                return HttpNotFound();
+            }
+            return View(czytelnik);
+        }
+
+        // POST: Czytelnik/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Imie,Nazwisko,Uzytkownik,Haslo,Email")] Czytelnik czytelnik)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(czytelnik).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(czytelnik);
+        }
+
+        // GET: Czytelnik/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Czytelnik czytelnik = db.Czytelnik.Find(id);
+            if (czytelnik == null)
+            {
+                return HttpNotFound();
+            }
+            return View(czytelnik);
+        }
+
+        // POST: Czytelnik/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Czytelnik czytelnik = db.Czytelnik.Find(id);
+            db.Czytelnik.Remove(czytelnik);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
