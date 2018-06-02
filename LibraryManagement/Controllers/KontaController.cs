a1cb0b2efa4250b820f1c529ce244a473b49ee97
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-06-02 14:30:26 +0200

    Dodanie kontrolera Konta

---
+++ LibraryManagement/Controllers/KontaController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using LibraryManagement.Models;
+
+namespace LibraryManagement.Controllers
+{
+
+    public class KontaController : Controller
+    {
+        private LibraryManagementDataEntities context = new LibraryManagementDataEntities();
+        // GET: Login
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Get
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Register(Czytelnik czytelnik)
+        {
+
+            if (ModelState.IsValid)
+            {
+                context.Czytelnik.Add(czytelnik);
+            }
+            return View(czytelnik);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Czytelnik czytelnik)
+        {
+            if (ModelState.IsValid)
+            {
+                var currentuser = context.Czytelnik.Where(user => user.Uzytkownik.Equals(czytelnik.Uzytkownik) && user.Haslo.Equals(czytelnik.Haslo)).FirstOrDefault();
+                if (currentuser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(currentuser.ID.ToString(), false);
+                    Session["UserID"] = currentuser.ID.ToString();
+                    return RedirectToAction("Index", "Czytelnicy");
+                }
+            }
+
+            return View(czytelnik);
+        }
+
+        [HttpGet]
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
